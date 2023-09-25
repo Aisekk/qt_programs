0222d581ff4f92ce0e8bf3d09f9bbba22065354d
Alexander Chekmarev <gooletters@gmail.com>
2023-09-25 23:18:19 +0300

    updated heap_hash

--- draft/draft/hash_heap.h
+++ draft/draft/hash_heap.h
@@ -5,6 +5,7 @@
 #include <queue>
 #include <string>
 #include <unordered_set>
+#include <map>
 
 using namespace std;
 
@@ -13,6 +14,7 @@ namespace Heap {
 class Solution {
 public:
     using prior_queue = std::priority_queue<std::string, std::vector<std::string>, std::greater<std::string>>;
+
     vector<string> topKFrequent(vector<string>& words, int k) {
         std::unordered_multiset<string> ums;
         ums.reserve(words.size());
@@ -21,47 +23,43 @@ public:
         }
         vector<string> value;
         value.reserve(words.size());
-        //const int s = k;
-        while (k != 0) {
-            auto pq = build(k, ums);
-            push(pq, value);
-            --k;
+        std::map<int, prior_queue, std::greater<int>> ord;
+        //for (int i = 0; i < ums.bucket_count(); ++i) {
+        for (auto it = ums.begin(); it != ums.end();) {
+            //int bsize = (int)ums.bucket_size(i);
+            //const auto &word = *ums.begin(i);
+            const auto &word = *it;
+            int bsize = ums.count(word);
+            if (ord.find(bsize) == ord.end()) {
+                //ord.emplace(std::make_pair(bsize, {word}));
+                prior_queue pq;
+                pq.push(word);
+                ord[bsize] = pq;
+                auto p = ums.equal_range(word);
+                it = ums.erase(p.first, p.second);
+            } else {
+                if (ord[bsize].top() != word) {
+                    ord[bsize].push(word);
+                }
+                ++it;
+            }
+        }
+        for (auto it = ord.begin(); it != ord.end(); ++it) {
+            if (k == 0) {
+                break;
+            }
+            auto &[i, pq] = *it;
+            push(pq, value, k);
         }
         return value;
     }
 private:
-    prior_queue build(int k, std::unordered_multiset<string> &ums) {
-        prior_queue pq;
-        // for (auto it = ums.begin(); it != ums.end(); ++it) {
-        //     int index = ums.bucket(*it);
-        //     if (ums.count(*it) == k) {
-        //         const auto &word = *it;
-        //         pq.push(word);
-        //         it = ums.erase(word);
-        //     }
-        // }
-        //for (int i = 0; i < ums.bucket_count(); ++i) {}
-        for (auto it = ums.begin(); it != ums.end();) {
-            int index = ums.bucket(*it);
-            if ((int)ums.bucket_size(index) == k) {
-                const auto &word = *it;
-                auto p = ums.equal_range(word);
-                //const auto &word = *ums.begin(index);
-                pq.push(word);
-                it = ums.erase(p.first, p.second);
-            } else {
-                ++it;
+    void push(prior_queue &pq, vector<string> &value, int &k) {
+        for (; !pq.empty(); pq.pop(), --k) {
+            if (k == 0) {
+                break;
             }
-        }
-        return pq;
-    }
-    void push(prior_queue &pq, vector<string> &value) {
-        // for (; !pq.empty(); pq.pop()) {
-        //     value.push_back(pq.top());
-        // }
-        while (!pq.empty()) {
             value.push_back(pq.top());
-            pq.pop();
         }
     }
 };
