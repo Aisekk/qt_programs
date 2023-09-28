763da1d19ba9e6a4ef178a7517a575a41285fa6e
chekmarev.a <chekmarev.a@milandr.ru>
2023-09-28 09:16:13 +0300

    Top k frequent first solution

--- draft/draft/hash_heap.h
+++ draft/draft/hash_heap.h
@@ -6,6 +6,7 @@
 #include <string>
 #include <unordered_set>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -23,24 +24,24 @@ public:
         }
         vector<string> value;
         value.reserve(words.size());
-        std::map<int, prior_queue, std::greater<int>> ord;
+        //std::map<int, prior_queue, std::greater<int>> ord;
+        std::map<int, vector<string>, std::greater<int>> ord;
         //for (int i = 0; i < ums.bucket_count(); ++i) {
         for (auto it = ums.begin(); it != ums.end();) {
             //int bsize = (int)ums.bucket_size(i);
             //const auto &word = *ums.begin(i);
             const auto &word = *it;
-            int bsize = ums.count(word);
+            int bsize = (int)ums.count(word);
             if (ord.find(bsize) == ord.end()) {
                 //ord.emplace(std::make_pair(bsize, {word}));
-                prior_queue pq;
-                pq.push(word);
-                ord[bsize] = pq;
+                //prior_queue pq;
+                //pq.push(word);
+                vector<string> v = {word};
+                ord[bsize] = v;
                 auto p = ums.equal_range(word);
                 it = ums.erase(p.first, p.second);
             } else {
-                if (ord[bsize].top() != word) {
-                    ord[bsize].push(word);
-                }
+                ord[bsize].push_back(word);
                 ++it;
             }
         }
@@ -48,12 +49,30 @@ public:
             if (k == 0) {
                 break;
             }
-            auto &[i, pq] = *it;
-            push(pq, value, k);
+            auto &[i, v] = *it;
+            auto last = std::unique(v.begin(), v.end());
+            v.erase(last, v.end());
+            std::sort(v.begin(), v.end());
+            last = std::unique(v.begin(), v.end());
+            v.erase(last, v.end());
+            push(v, value, k);
         }
         return value;
     }
 private:
+    void push(vector<string> &v, vector<string> &value, int &k) {
+        while (!v.empty()) {
+            if (k == 0) {
+                break;
+            }
+            std::make_heap(v.begin(), v.end(), std::greater<>{});
+            std::pop_heap(v.begin(), v.end(), std::greater<>{});
+            auto top = v.back();
+            v.pop_back();
+            value.push_back(top);
+            --k;
+        }
+    }
     void push(prior_queue &pq, vector<string> &value, int &k) {
         for (; !pq.empty(); pq.pop(), --k) {
             if (k == 0) {
